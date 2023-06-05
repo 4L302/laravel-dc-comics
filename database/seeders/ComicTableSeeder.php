e9a98f162d4aac62f6a7b7c68736aa68d0f7bb0b
4L302 <alegarg2002@gmail.com>
2023-06-05 17:19:43 +0200

    agg seeder

--- database/seeders/ComicTableSeeder.php
+++ database/seeders/ComicTableSeeder.php
@@ -6,7 +6,7 @@ use App\Models\Comic;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-use Faker\Generator as Faker;
+/* use Faker\Generator as Faker; */
 
 class ComicTableSeeder extends Seeder
 {
@@ -15,7 +15,7 @@ class ComicTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run(Faker $faker)
+    /* public function run(Faker $faker)
     {
         for($i = 0; $i < 20; $i ++){
             $newComic = new Comic();
@@ -26,6 +26,22 @@ class ComicTableSeeder extends Seeder
             $newComic -> Series = $faker->sentence(3);
             $newComic -> Sale_date = $faker->dateTime();
             $newComic -> Type = $faker->word();
+            $newComic->save();
+        }
+    } */
+
+    public function run ()
+    {
+        $comics = config('db.comics');
+        foreach ($comics as $comic) {
+            $newComic = new Comic();
+            $newComic -> Title = $comic ['title'];
+            $newComic -> Description = $comic ['description'];
+            $newComic -> Thumb = $comic ['thumb'];
+            $newComic -> Price = $comic ['price'];
+            $newComic -> Series = $comic ['series'];
+            $newComic -> Sale_date = $comic ['sale_date'];
+            $newComic -> Type = $comic ['type'];
             $newComic->save();
         }
     }
